ca2581a57a8fbe800ba2a499de1b6326632c3fc5
tomjawhoon <naratorn_152@hotmail.com>
2020-03-12 15:38:36 +0700

    edit transection functiom success

--- test111.js
+++ test111.js
@@ -1,45 +1,24 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const Web3 = require('web3');
-const fs = require('fs');
-const path = require('path');
-const infura = {
-    projectId: '37dd526435b74012b996e147cda1c261',
-    projectSecret: '55c6430534c042a1b762cd5f6e0f0a55',
-    endpoint: "wss://kovan.infura.io/ws/v3/37dd526435b74012b996e147cda1c261"
-    
-}
-const contractAddress = "0x0d01bc6041ac8f72e1e4b831714282f755012764";
-const router = express.Router();
-const app = express();
-const web3 = new Web3(infura.endpoint);
-const abi = JSON.parse(fs.readFileSync(path.resolve(__dirname, './abi.json'), 'utf-8'));
-const contract = new web3.eth.Contract(abi,contractAddress);
-app.use(cors());
-app.use('/api', bodyParser.json(), router);   //[use json]
-app.use('/api', bodyParser.urlencoded({ extended: false }), router);
-
-router.route('/')
-    .get(async (req, res) => {
-        try {
-            const events = await contract.getPastEvents('Transfer', {
-                filter: { _to: '0x07E4590839ed31A7ad3c9Fb7807Df67eADD5A230' },
-                fromBlock: 0,
-                toBlock: 'latest'
-            });
-            let transactions = {}
-            events.forEach((event) => {
-                const { transactionHash, returnValues } = event;
-                console.log(returnValues);
-                const { _from, _to, _value } = returnValues;
-                transactions[transactionHash] = { from: _from, to: _to, value: _value }
-               // console.log(transactions);
-            })
-            res.json(transactions)
-        } catch (e) {
-            console.error(e);
-            res.json(e)
-        }
-    });
-app.listen(80, () => console.log('server is ready'))
+const ws = "wss://kovan.infura.io/ws/v3/37dd526435b74012b996e147cda1c261";
+// wss://kovan.infura.io/ws/v3/37dd526435b74012b996e147cda1c261
+const web3 = new Web3(ws);
+web3.eth.subscribe("pendingTransactions", (error, result) => {
+    if(!error) {
+        var wallet = "0x4798578d82Cf35919c24BB6bf9392d1e48ca6d7C"
+        // 0x483b84aeFabD6f19eba02F3fA078a9c2B489F1e2
+        web3.eth.getTransaction(result)
+            .then( (transaction)  =>{
+               if(transaction !== null && transaction.to) {
+                //console.log("transaction  ===>",transaction)
+                console.log("transaction.to",transaction.to)
+                //console.log("transaction.to  ===>",transaction.to.toLocaleLowerCase())
+                   if((transaction.to).toLocaleLowerCase() === wallet) {
+                
+                   console.log(`
+                     from: ${transaction.from}  
+                     amount: ${web3.utils.fromWei(transaction.value, 'ether')}`)
+                   }
+               }
+        });
+    }
+});
